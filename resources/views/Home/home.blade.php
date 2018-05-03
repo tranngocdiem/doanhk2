5cdc42502445812e13edf6b3380f0820cf4194b6
tranngocdiem <tranngocdiem@users.noreply.github.com>
2018-05-03 19:20:08 +0700

    web
    web

---
+++ resources/views/Home/home.blade.php
@@ -0,0 +1,174 @@
+@extends('_layout')
+@section('title') 
+Trang chủ
+@stop
+@section('content')
+    <!--<section class="jumbotron text-center" id="album">
+      <div class="container">
+        <h1 class="jumbotron-heading" style="font-family: Old Standard TT;font-style:italic;"><strong>TỔ ẤM CỦA NGƯỜI TINH TẾ</strong></h1>
+        <p class="lead text-muted" >TỔ ẤM CỦA NGƯỜI TINH TẾ</p>
+        <p>
+          <a href="#" class="btn btn-primary my-2">Main call to action</a>
+          <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+        </p>
+      </div>
+    </section>-->
+    <div style="background-image: linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(image/background.jpg);
+  background-size:cover;height: 100vh;">
+    <div class="contain">
+      <nav class="row2">
+        <div class="">
+          <h1 class="m-b-20"><strong>DTQ FURNITURE</strong></h1>
+          <p>
+            <a class="btn btn-primary btn-xl js-scroll-trigger" href="#album-list">BỘ SƯU TẬP</a>
+          </p>
+        </div>
+      </nav>
+    </div>
+
+    </div>
+    <div class="album py-5 bg-light" id="album-list">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top" src="image/album1.jpg" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Ảnh 1</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                  </div>
+                  <small class="text-muted">9 mins</small>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                  </div>
+                  <small class="text-muted">9 mins</small>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                  </div>
+                  <small class="text-muted">9 mins</small>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                  </div>
+                  <small class="text-muted">9 mins</small>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                  </div>
+                  <small class="text-muted">9 mins</small>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                  </div>
+                  <small class="text-muted">9 mins</small>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                  </div>
+                  <small class="text-muted">9 mins</small>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                  </div>
+                  <small class="text-muted">9 mins</small>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                  </div>
+                  <small class="text-muted">9 mins</small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+@stop
---
+++ resources/views/_layout.blade.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>@section('title') Nội thất @show</title>
+  @section('link')
+  <link rel="stylesheet" href="{{ url('/') }}./css/Home/home.css">
+  <link rel="stylesheet" href="{{ url('/') }}./css/bootstrap.min.css">
+  <link rel="stylesheet" href="{{ url('/') }}./web-fonts-with-css/css/fontawesome-all.min.css">
+  @show
+  <script src="{{ url('/') }}./js/jquery-3.3.1.min.js"></script>
+  <script src="{{ url('/') }}./js/popper.min.js"></script>
+  <script src="{{ url('/') }}./js/bootstrap.min.js"></script>
+</head>
+<body>
+   <!-- header!-->
+  <header>
+    <div>
+      <div class="hide-content-1" aria-hidden="true">
+        <nav class="sidebar">
+          
+          <div class="menu">
+            <a href="#">
+              <img class="logo" src="{{ url('/') }}./image/logo.png" style="width: 50px;height: 50px;" alt="" id="logo"/>
+            </a>
+
+            <ul>
+              <li><a href="">Trang chủ</a></li>
+              <li><a href="">Dịch vụ</a></li>
+              <li><a href="">Sản phẩm</a></li>
+              <li><a href="">Liên hệ</a></li>
+              <li><a href="">Tin tức</a></li>
+              <li><a href="">Trợ giúp</a></li>
+            </ul>
+            <ul class="social-icon" >
+              <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+              <li><a href=""><i class="fab fa-twitter"></i></a></li>
+              <li><a href=""><i class="fab fa-google-plus-g"></i></a></li>
+              <li><a href=""><i class="fab fa-linkedin-in"></i></a></li>
+              <li><a href=""><i class="fab fa-instagram"></i></a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+      <div class="row1">
+        <div class="">
+
+          <div class="menu-icon">
+            <span class="fa fa-bars fa-2x"></span>
+          </div>
+
+
+          <div class="backgrSearch">
+            <form class="mainForm">
+
+              <button class="btn-select">
+                <select>
+                  <option>All</option>
+                </select>
+              </button>
+
+              <input type ="text" name = "search" placeholder = "Search">
+              <button class="btn-search"><i class="fas fa-search"></i></button>
+
+            </form>
+
+
+          </div>
+          <div>
+            <ul class="cart-icon">
+              <li><a href="" class="fas fa-shopping-cart""><i>Giỏ hàng</i></a></li>
+              <li><a href="" class="fas fa-cart-arrow-down"><i>Sản phẩm mới</i></a></li>
+              <li class="icon3"><a href="" class="fas fa-sign-in-alt"><i>Đăng nhập</i></a></li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </div>
+   
+  </header>
+  <!-- end header!-->
+  <div class = "content">
+
+  	@yield('content')
+
+  </div>
+    <footer id="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading">Liên hệ</h2>
+            <hr class="my-4">
+            <p class="mb-5">Địa chỉ: DTQfurniture, khu phố 6, phường Linh Trung, quận Thủ Đức, TPHCM</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 ml-auto text-center">
+            <i class="fa fa-phone fa-3x mb-3 sr-contact" data-sr-id="8" style="; visibility: visible;  -webkit-transform: scale(1); opacity: 1;transform: scale(1); opacity: 1;-webkit-transition: -webkit-transform 0.6s cubic-bezier(0.6, 0.2, 0.1, 1) 0s, opacity 0.6s cubic-bezier(0.6, 0.2, 0.1, 1) 0s; transition: transform 0.6s cubic-bezier(0.6, 0.2, 0.1, 1) 0s, opacity 0.6s cubic-bezier(0.6, 0.2, 0.1, 1) 0s; "></i>
+            <p>123-456-6789</p>
+          </div>
+          <div class="col-lg-4 mr-auto text-center">
+            <i class="fa fa-envelope-o fa-3x mb-3 sr-contact" data-sr-id="9" style="; visibility: visible;  -webkit-transform: scale(1); opacity: 1;transform: scale(1); opacity: 1;-webkit-transition: -webkit-transform 0.6s cubic-bezier(0.6, 0.2, 0.1, 1) 0s, opacity 0.6s cubic-bezier(0.6, 0.2, 0.1, 1) 0s; transition: transform 0.6s cubic-bezier(0.6, 0.2, 0.1, 1) 0s, opacity 0.6s cubic-bezier(0.6, 0.2, 0.1, 1) 0s; "></i>
+            <p>
+              <a href="mailto:your-email@your-domain.com">dtqfurniture2gmail.com</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  // file js của từng trang
+  @section('js')
+  <script type="text/javascript">
+
+      // Menu-toggle button
+
+
+      $(document).ready(function(){
+        $(".menu-icon").click( function(){
+          $(".menu").toggleClass("showing");
+          $(".row1").toggleClass("active");
+
+
+        });
+        //toggle class sẽ chuyển tắt nếu gọi nó 1 lần nữa
+        $( ".menu" ).mouseleave(function() {
+          $(".menu").toggleClass("showing");
+          $(".row1").toggleClass("active");
+
+        });
+        
+        
+      });
+
+      // Scrolling Effect
+
+      $(window).on("scroll", function() {
+        if($(window).scrollTop()) {
+          $(".row1").addClass("black");
+
+        }
+
+        else {
+          $(".row1").removeClass("black");
+        }
+      });
+      // Select all links with hashes
+      $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+      ) {
+      // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: (target.offset().top-77)
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+</script>
+@show
+
+</body>
+</html>
+
